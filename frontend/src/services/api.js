341a9696a9c4678a6dfb2a0e57e983fb31fbc6c0
sunkaramahesh09 <sunkaramahesh494@gmail.com>
2026-04-03 22:50:35 +0530

    Fix API baseURL for deployed environment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL.replace(/\/+$/, '') + '/api' : '/api',
   timeout: 30000,
   headers: { 'Content-Type': 'application/json' },
 })
